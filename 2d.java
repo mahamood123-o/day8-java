0e8935183aa5ad6cc7db3a80a252ee8bda6a6675
Mahamood <shaikmisba305@gmail.com>
2024-12-09 13:47:28 +0530

    Add files via upload

---
+++ 2d.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        int a[][]={{1,2},{3,4}};
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
